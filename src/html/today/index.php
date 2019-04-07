697aeda16d68f3aa355364590112330336aadd34
Ryder Damen <ryder@benchsci.com>
2019-04-07 13:19:54 -0400

    updating relative import to above web root

--- src/html/today/index.php
+++ src/html/today/index.php
@@ -7,7 +7,7 @@
 	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
-	include('../includes/config.php');
+	include('../../includes/config.php');
 		
 	
 	// Only display the page, if it has been enabled
